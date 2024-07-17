408e0eae9fcce7556b91b12b6d478933ca55e9ee
greats3an <greats3an@gmail.com>
2024-07-17 09:24:30 +0800

    自动化字库构建（！！）

--- build.ps1
+++ build.ps1
@@ -3,27 +3,50 @@ if ($args -notcontains '--no-update') {
     
     Remove-Item -Path ".bin" -Recurse -ErrorAction Ignore | Out-Null
     New-Item -ErrorAction Ignore -ItemType Directory -Path ".bin" | Out-Null
+    # MgsFontGen-DX
+    Invoke-WebRequest -Uri "https://github.com/mos9527/mgsfontgen-dx/releases/download/latest/mgsfontgen-dx.zip" -OutFile .\.bin\mgsfontgen-dx.zip
+    Expand-Archive -Path .\.bin\mgsfontgen-dx.zip -DestinationPath .\.bin\mgsfontgen-dx
     # MgsScriptTools
-    Invoke-WebRequest -Uri "https://github.com/mos9527/MgsScriptTools/releases/download/latest/MgsScriptTools.zip" -OutFile .\.bin\MgsScriptTools.zip
+    Invoke-WebRequest -Uri "https://github.com/mos9527/MgsScriptTools/releases/download/latest/net6.0.zip" -OutFile .\.bin\MgsScriptTools.zip
     Expand-Archive -Path .\.bin\MgsScriptTools.zip -DestinationPath .\.bin\MgsScriptTools    
     # cpk
     Invoke-WebRequest -Uri "https://github.com/mos9527/mages-tools/releases/download/latest/cpk.exe" -OutFile .\.bin\cpk.exe
     # LanguageBarrier
     Invoke-WebRequest -Uri "https://github.com/mos9527/LanguageBarrier/releases/download/latest/dinput8.dll" -OutFile .\.bin\dinput8.dll
 }
+# TODO: CoZ file hook
+$GamePath = "H:\Etc\SteamLibrary\steamapps\common\CHAOS;HEAD NOAH"
 
 Remove-Item -Path ".temp" -Recurse -ErrorAction Ignore | Out-Null
 New-Item -ErrorAction Ignore -ItemType Directory -Path ".temp" | Out-Null
 
+Write-Host "Building Charset"
+$CharsetPath = ".bin\MgsScriptTools\mgs-spec-bank\charset\chaos_head_noah-zhs.utf8"
+python .\scripts\build_charset.py .\scripts\mes01 $CharsetPath
+python .bin\MgsScriptTools\mgs-spec-bank\charset\generate_from_charset.py $CharsetPath .bin\MgsScriptTools\mgs-spec-bank\charset\chaos_head_noah-zhs.json
+
+Set-Location -Path .temp
+..\.bin\mgsfontgen-dx\bin\Release\net461\mgsfontgen-dx.exe generate --charset ..\$CharsetPath --compound-characters ..\.bin\mgsfontgen-dx\chn\CompoundCharacters.tbl --font-family "VL PGothic" --image-format png
+
+Set-Location -Path ..
 Write-Host "Compiling scripts"
-Set-Location scripts
-cmd /c invoke.cmd compile
+.bin\MgsScriptTools\MgsScriptTools.exe compile --bank-directory .bin\MgsScriptTools\mgs-spec-bank --compiled-directory .temp\mes01 --decompiled-directory scripts\mes01 --string-syntax Sc3Tools --charset chaos_head_noah-zhs  --flag-set chaos_head_windows --instruction-sets base,chaos_head_noah
+
+Write-Host "Unpacking system.cpk"
+.bin\cpk.exe -i $GamePath\Data\system.cpk -o .temp\system
+
+Write-Host "Replacing font maps"
+Copy-Item -Path ".temp\FONT_A.png" -Destination .temp\system\20 -Recurse -Force
+Copy-Item -Path ".temp\FONT_A.png" -Destination .temp\system\21 -Recurse -Force
+Copy-Item -Path ".temp\FONT_A.png" -Destination .temp\system\22 -Recurse -Force
+Copy-Item -Path ".temp\FONT_A.png" -Destination .temp\system\23 -Recurse -Force
+
+Write-Host "Repacking system.cpk"
+.bin\cpk.exe -r .temp\system.cpk -o .temp\system
 
 Write-Host "Packing script CPKs"
-Set-Location ..\.temp
-..\.bin\cpk.exe -r mes01.cpk -o ..\.temp\mes01
+.bin\cpk.exe -r .temp\mes01.cpk -o .temp\mes01
 
-Set-Location ..
-
-# TODO: Implement CoZ's file hook here. Testing for now.
-Copy-Item -Path ".temp\mes01.cpk" -Destination "H:\Etc\SteamLibrary\steamapps\common\CHAOS;HEAD NOAH\Data\mes01.cpk" -Recurse -Force
+Write-Host "Writing back CPKs"
+Copy-Item -Path ".temp\system.cpk" -Destination $GamePath\Data\system.cpk -Recurse -Force
+Copy-Item -Path ".temp\mes01.cpk" -Destination $GamePath\Data\mes01.cpk -Recurse -Force
---
+++ scripts/build_charset.py
@@ -0,0 +1,12 @@
+import sys, os
+if len(sys.argv) != 3:
+    print('Usage: python build_charset.py <input_dir> <output_file>')
+    sys.exit(1)
+charset = set()
+for root, dirs, files in os.walk(sys.argv[1]):
+    for file in files:        
+        file_path = os.path.join(root, file)
+        for c in open(file_path, 'r', encoding='utf-8').read(): charset.add(c)
+charset.remove('\n')
+open(sys.argv[2], 'w', encoding='utf-8').write(''.join(sorted(charset)))
+print('Found %d characters' % len(charset))
