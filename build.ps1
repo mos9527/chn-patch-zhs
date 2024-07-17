4983b2fcf90219be07cd5a3cc1037852192d7a8b
greats3an <greats3an@gmail.com>
2024-07-17 10:27:20 +0800

    添加额外 CoZ 补丁内容

--- build.ps1
+++ build.ps1
@@ -12,18 +12,19 @@ if ($args -notcontains '--no-update') {
     # cpk
     Invoke-WebRequest -Uri "https://github.com/mos9527/mages-tools/releases/download/latest/cpk.exe" -OutFile .\.bin\cpk.exe
     # LanguageBarrier
-    Invoke-WebRequest -Uri "https://github.com/mos9527/LanguageBarrier/releases/download/latest/dinput8.dll" -OutFile .\.bin\dinput8.dll
+    Invoke-WebRequest -Uri "https://github.com/mos9527/LanguageBarrier/releases/download/latest/dinput8_chn.dll" -OutFile .\.bin\dinput8.dll
 }
 
 Remove-Item -Path ".temp" -Recurse -ErrorAction Ignore | Out-Null
 New-Item -ErrorAction Ignore -ItemType Directory -Path ".temp" | Out-Null
 
-Remove-Item -Path ".dist" -Recurse -ErrorAction Ignore | Out-Null
-New-Item -ErrorAction Ignore -ItemType Directory -Path ".dist" | Out-Null
+Remove-Item -Path "dist" -Recurse -ErrorAction Ignore | Out-Null
+New-Item -ErrorAction Ignore -ItemType Directory -Path "dist" | Out-Null
 
 Write-Host "Building Charset"
 $CharsetPath = ".bin\MgsScriptTools\mgs-spec-bank\charset\chaos_head_noah-zhs.utf8"
 python .\scripts\build_charset.py .\scripts\mes01 $CharsetPath
+python .bin\MgsScriptTools\mgs-spec-bank\charset\generate_from_charset.py $CharsetPath .bin\MgsScriptTools\mgs-spec-bank\charset\chaos_head_noah-zhs.json
 
 Write-Host "Compiling scripts"
 .bin\MgsScriptTools\MgsScriptTools.exe compile --bank-directory .bin\MgsScriptTools\mgs-spec-bank --compiled-directory .temp\mes01 --decompiled-directory scripts\mes01 --string-syntax Sc3Tools --charset chaos_head_noah-zhs  --flag-set chaos_head_windows --instruction-sets base,chaos_head_noah
@@ -31,15 +32,21 @@ Write-Host "Compiling scripts"
 Write-Host "Packing script CPKs"
 .bin\cpk.exe -r .temp\mes01.cpk -o .temp\mes01
 
-Copy-Item -Path config\*.json -Destination dist\LanguageBarrier\  -Recurse -Force
+Write-Host "Copying files"
+New-Item -ErrorAction Ignore -ItemType Directory -Path "dist\LanguageBarrier" | Out-Null
+New-Item -ErrorAction Ignore -ItemType Directory -Path "dist\LanguageBarrier\fonts" | Out-Null
+New-Item -ErrorAction Ignore -ItemType Directory -Path "dist\HEAD NOAH" | Out-Null
+Copy-Item -Path ".temp\mes01.cpk" -Destination dist\LanguageBarrier\c0mes01.cpk -Recurse -Force
+Copy-Item -Path "data\fonts\*" -Destination dist\LanguageBarrier\fonts\ -Recurse -Force
+Copy-Item -Path "data\dll\*" -Destination "dist\HEAD NOAH" -Recurse -Force
+Copy-Item -Path ".bin\dinput8.dll" -Destination "dist\HEAD NOAH\dinput8.dll" -Recurse -Force
+
+Copy-Item -Path config\*.json -Destination dist\LanguageBarrier\ -Recurse -Force
+Copy-Item -Path config\*.bin -Destination dist\LanguageBarrier\ -Recurse -Force
 Write-Host "Updating config"
 python config\update_config.py $CharsetPath dist\LanguageBarrier\patchdef.json
 
-Write-Host "Copying files"
-Copy-Item -Path ".temp\mes01.cpk" -Destination dist\LanguageBarrier\cmes01.cpk -Recurse -Force
-Copy-Item -Path "data\*" -Destination dist\LanguageBarrier\ -Recurse -Force
-Copy-Item -Path ".bin\dinput8.dll" -Destination "dist\dinput8.dll" -Recurse -Force
-$GamePath = "H:\Etc\SteamLibrary\steamapps\common\CHAOS;HEAD NOAH"
+$GamePath = "C:\Etc\SteamLibrary\steamapps\common\CHAOS;HEAD NOAH"
 if (Test-Path $GamePath) {
     Write-Host "Copying to game directory"
     Copy-Item -Path "dist\*" -Destination "$GamePath" -Recurse -Force
