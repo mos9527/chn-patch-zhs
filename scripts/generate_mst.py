ec263c91b7a008b6b89243c3cc6e0f72d4fa1924
greats3an <greats3an@gmail.com>
2024-07-18 09:33:39 +0800

    更新 mst codegen

--- scripts/generate_mst.py
+++ scripts/generate_mst.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 if len(sys.argv) != 3:
     print('Usage: python generate_mst.py  <output dir> <input dir>')
     sys.exit(1)
+id_index = 'msgid '
 language_index = 'msgstr '
 for root, dirs, files in os.walk(sys.argv[2]):
     for file in files:
@@ -11,9 +12,10 @@ for root, dirs, files in os.walk(sys.argv[2]):
             output_path = os.path.join(sys.argv[1], file.split('.')[0] + '.mst')
             with open(file_path, 'r', encoding='utf-8') as fin:
                 with open(output_path, 'w', encoding='utf-8') as fout:
-                    for line in fin.readlines():
-                        if str(line).startswith(language_index):
-                                line = line[len(language_index)+1:-2]
-                                if line:
-                                    fout.write(line) # +1 for the space, -2 for the quotes and NL
-                                    fout.write('\n')
+                    lines = fin.readlines()
+                    ids = (line[len(id_index)+1:-2] for line in lines if str(line).startswith(id_index))
+                    strs = (line[len(language_index)+1:-2] for line in lines if str(line).startswith(language_index))
+                    for iid, sstr in zip(ids, strs):
+                        if iid:
+                            if sstr: fout.write('%s\n' % sstr)
+                            else: fout.write('%s\n' % iid)
